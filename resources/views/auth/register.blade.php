c57dc733c4ed6fb6607f4d45f84bd51c95a2a89e
Elian-D <eliandavidbertrecuello19@gmail.com>
2025-11-27 21:55:06 -0400

    style: register adaptado al diseño

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -2,51 +2,66 @@
     <form method="POST" action="{{ route('register') }}">
         @csrf
 
+    <div class="text-center">
+        <h2 class="font-black text-lg lg:text-2xl">Registrate</h2>
+        <h3 class="text-gray-400 text-xs lg:text-lg">Ingresa tus credenciales para continuar</h3>
+    </div>
         <!-- Name -->
         <div>
-            <x-input-label for="name" :value="__('Name')" />
-            <x-text-input id="name" class="block mt-1 w-full" type="text" name="name" :value="old('name')" required autofocus autocomplete="name" />
-            <x-input-error :messages="$errors->get('name')" class="mt-2" />
+            <x-input-label for="name" :value="__('Nombre')" />
+            <x-text-input id="name" class="block mt-1 w-full" type="text" name="name" :value="old('name')" required autofocus autocomplete="name" placeholder="Juan Pablo" />
+            <x-input-error :messages="$errors->get('name')" class="mt-2"/>
         </div>
 
         <!-- Email Address -->
         <div class="mt-4">
-            <x-input-label for="email" :value="__('Email')" />
-            <x-text-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required autocomplete="username" />
-            <x-input-error :messages="$errors->get('email')" class="mt-2" />
+            <x-input-label for="email" :value="__('Correo')" />
+            <x-text-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required autocomplete="username" placeholder="ejemplo@gmail.com"/>
+            <x-input-error :messages="$errors->get('email')" class="mt-2"/>
         </div>
 
         <!-- Password -->
         <div class="mt-4">
-            <x-input-label for="password" :value="__('Password')" />
+            <x-input-label for="password" :value="__('Contraseña')" />
 
             <x-text-input id="password" class="block mt-1 w-full"
                             type="password"
                             name="password"
-                            required autocomplete="new-password" />
+                            required autocomplete="new-password" 
+                            placeholder="•••••••••" />
+                            
 
             <x-input-error :messages="$errors->get('password')" class="mt-2" />
         </div>
 
         <!-- Confirm Password -->
         <div class="mt-4">
-            <x-input-label for="password_confirmation" :value="__('Confirm Password')" />
+            <x-input-label for="password_confirmation" :value="__('Confirma contraseña')" />
 
             <x-text-input id="password_confirmation" class="block mt-1 w-full"
                             type="password"
-                            name="password_confirmation" required autocomplete="new-password" />
+                            name="password_confirmation" required autocomplete="new-password" 
+                            placeholder="•••••••••"/>
 
             <x-input-error :messages="$errors->get('password_confirmation')" class="mt-2" />
         </div>
 
-        <div class="flex items-center justify-end mt-4">
+        <div class="mt-2">
             <a class="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500" href="{{ route('login') }}">
-                {{ __('Already registered?') }}
+                {{ __('¿Estás registrado?') }}
             </a>
+        </div>
 
-            <x-primary-button class="ms-4">
+        <div class="flex items-center justify-end mt-4">
+
+           <x-primary-button class=" justify-center bg-custom-gradient-2 ms-3 w-full text-center ms-0 hover:text-blue-200">
                 {{ __('Register') }}
             </x-primary-button>
+        </div>
+        <div>
+            <p class="mt-6 text-center text-sm text-gray-400">
+                © 2025 Suportec Network SRL · Todos los derechos reservados
+            </p>
         </div>
     </form>
 </x-guest-layout>
