2203aa264fc6e8d368e71a348d02663e6e961a24
Elian-D <eliandavidbertrecuello19@gmail.com>
2025-12-11 11:45:00 -0400

    feat(config): crear archivo de rutas configuration.php y definir rutas

---
+++ routes/configuration.php
@@ -0,0 +1,18 @@
+<?php
+
+use App\Http\Controllers\Configuration\TipoDocumentoController;
+use Illuminate\Support\Facades\Route;
+
+
+
+// Rutas de documentos
+Route::middleware(['auth', 'permission:manage documents'])->group(function () {
+    Route::resource('tipos-documentos', TipoDocumentoController::class);
+});
+
+// Rutas de ubicación
+/* Route::middleware(['auth', 'permission:manage locations'])->group(function () {
+    Route::resource('provincias', ProvinciaController::class);
+    Route::resource('municipios', MunicipioController::class);
+    Route::resource('sectores', SectorController::class);
+}); */
