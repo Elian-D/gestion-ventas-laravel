1a69f34bc83035ec01c89ba2cf5468938045914a
Elian-D <eliandavidbertrecuello19@gmail.com>
2025-12-11 18:55:54 -0400

    fix(config): ruta del controlador arreglada

--- app/Http/Controllers/Configuration/TipoDocumentoController.php
+++ app/Http/Controllers/Configuration/TipoDocumentoController.php
@@ -1,7 +1,8 @@
 <?php
 
-namespace App\Http\Controllers;
+namespace App\Http\Controllers\Configuration;
 
+use App\Http\Controllers\Controller;
 use App\Models\TipoDocumento;
 use Illuminate\Http\Request;
 
