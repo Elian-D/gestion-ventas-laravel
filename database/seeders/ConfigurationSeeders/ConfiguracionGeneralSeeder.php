e4fb3e26900073afd0d2a65aad2b6a98615ceb8f
Elian-D <eliandavidbertrecuello19@gmail.com>
2025-12-16 16:31:23 -0400

    feat(configuracion-general): crear modelo, migración y controlador

---
+++ database/seeders/ConfigurationSeeders/ConfiguracionGeneralSeeder.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Database\Seeders\ConfigurationSeeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class ConfiguracionGeneralSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        //
+    }
+}
